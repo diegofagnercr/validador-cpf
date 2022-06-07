f8419ce2264968369c38303f5721bcf0961c0399
diegofagnercr <diegofagner.cr@gmail.com>
2022-06-07 16:52:01 -0300

    Inclusão do Bootstrap, e criação do style.css e scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+console.log('Javascript carregado!')
+
+function validaCPF(cpf) {
+    console.log('Entro na validação!')    
+}
+
+function validacao() {
+console.log('Iniciando validação do CPF!')
+
+var cpf = document.getElementById('input_cpf').value;
+console.log(cpf)
+
+var resultadoValidacao = validaCPF(cpf)    
+}
